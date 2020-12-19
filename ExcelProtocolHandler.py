52de276b01d4e790b6d51b8f242f68ca210c0489
二氢茉莉酮酸甲酯 <gao1021229073@live.com>
2020-12-19 12:39:46 +0800

    Simple adjust

--- ExcelProtocolHandler.py
+++ ExcelProtocolHandler.py
@@ -1,24 +1,28 @@
 import xlrd
-import csv
+import csv  
 import sys
 import os
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("illegal argument, try again.")
-        #exit(-1)
     elif len(sys.argv[1]) == 0:
         print("illegal argument, try again.")
-        #exit(-1)
     elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
         print(""" 
 Excel file handler
 
-    A simple executable program which helps transformed into csv or other file,
-    which is much more simple to analysis.
+    A simple program which helps transformed into csv or other file,
+     which is much more simple to analysis.Sheets will be saved as name '... - SheetN.csv'
+     this will create some files to storage all data
+    Due to some problems in Microsoft Excel (R), csv file is encoded as UTF-8 BOM, which
+     program cannot write with, you may need to trans file from utf-8 to utf-8 BOM.
 
-    This is an executable version which disabled the exit() code.
-    For opensource version, please check github.
+    For any issues or bugs, please go to github and report.
+
+    This is an executable version (winPE-32/.exe) which disabled the exit() code.
+    For open-source version, please check 
+    https://github.com/Holit/XlsxToCsv
 -----------------------Copyright (c) 2020 Jerry-------------------------------
 
 usage: eph [-h|-c|-p|--help|--csv|--print] {filename}
@@ -32,17 +36,12 @@ usage: eph [-h|-c|-p|--help|--csv|--print] {filename}
     elif sys.argv[1] == "--csv" or sys.argv[1] == "-c":
         if len(sys.argv) != 3:
             print("illegal input file : null file")
-            #exit(-2)
         elif not(os.path.exists(sys.argv[2])):
             print("illegal input file : there's no such file named " + sys.argv[2])
-            #exit(-3)
-        #This is not strict for win32 platform, which may cause errors.
         elif not(os.access(sys.argv[2],os.R_OK)):
             print("illegal input file : access failed at " + sys.argv[2])
-            #exit(-4)
         elif not(os.path.splitext(sys.argv[2])[-1][1:] == "xlsx" or os.path.splitext(sys.argv[2])[-1][1:] == "xls"):
             print("illegal input file : is not a excel-based file")
-            #exit(-5)
         else:
             try:
                 workbook = xlrd.open_workbook(sys.argv[2])
@@ -55,27 +54,18 @@ usage: eph [-h|-c|-p|--help|--csv|--print] {filename}
                         os.remove(csv_file_name)
                     nrows = table.nrows
                     ncols = table.ncols
-                    # 跳过表头，从第一行数据开始读
                     for rows_read in range(1,nrows):
-                        #每行的所有单元格内容组成一个列表
                         row_value = []
                         for cols_read in range(ncols):
-                            #获取单元格数据类型
                             ctype = table.cell(rows_read, cols_read).ctype
-                            #获取单元格数据
                             nu_str = table.cell(rows_read, cols_read).value
-                            #判断返回类型
-                            # 0 empty,1 string, 2 number(都是浮点), 3 date, 4 boolean, 5 error
-                            #是2（浮点数）的要改为int
                             if ctype == 2:
                                 nu_str = int(nu_str)
                             row_value.append(nu_str)
-                        with open(csv_file_name, 'a', encoding='utf-8',newline='') as f: #newline=''不加会多空行
+                        with open(csv_file_name, 'a', encoding='utf-8',newline='') as f:
                             write = csv.writer(f)
                             write.writerow(row_value)
                     i = i +1
                 print('Succeed, proceed ' + str(i) + ' sheets \n You may need to use excel to trans encoding from utf-8 to utf-8 BOM')
-                #exit(0)
             except Exception as e:
                 print("Unexcepted error occured: " + str(e))
-                #exit(-6)
